301f896515970d26eebf33d23aa207481df1ad8a
Michal Ciborowski <mciborowski33@gmail.com>
2020-03-04 20:47:35 +0100

    connection with postgres

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const express = require('express');
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const { Client } = require('pg')
+
+const client = new Client({
+  user: 'root',
+  host: 'localhost',
+  database: 'iot',
+  password: 'Marcin',
+  port: 5432,
+})
 
 const port = 80;
 
@@ -10,3 +19,20 @@ const port = 80;
 server.listen(port, function(){
     console.log(`App listening on port ${port}!`);
 });
+
+client.connect();
+
+function init(){
+    client.query('SELECT * from test', (err, res) => {
+        if( !err ){
+            console.log(res.rowCount);
+            console.log(res.rows);
+            if( res.rowCount > 0 ){
+                console.log(res);
+            }
+        }else{
+            console.log(err);
+        }
+        client.end();
+    })
+}
