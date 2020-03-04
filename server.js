1d85320450af0c689fe386cac9c6b59411aaa074
Michal Ciborowski <mciborowski33@gmail.com>
2020-03-04 20:19:50 +0100

    basic server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 // server.js
+const express = require('express');
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const port = 80;
+
+//app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+server.listen(port, function(){
+    console.log(`App listening on port ${port}!`);
+});
