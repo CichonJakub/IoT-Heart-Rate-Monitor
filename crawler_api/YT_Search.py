27914a762d144cab90ce4d9edf07c8a31e19b86b
rozanna <rozanna.boczar@gmail.com>
2020-03-22 14:15:48 +0100

    update

--- crawler_api/YT_Search.py
+++ crawler_api/YT_Search.py
@@ -1,32 +1,70 @@
 from youtube_api import YouTubeDataAPI
-
-api_key = 'AIzaSyC1GsiEj0D0zcQAyayaaCNa8oXeh9vU2bw'
-yt = YouTubeDataAPI(api_key)
+import json
 
 
-def get_playlists():
-    playlists_ids = []
-    if yt.verify_key():
-        results = yt.search(parser=None, q="bts", max_results=10, search_type="playlist")
-        #print(results)
-        for result in results:
-            playlists_ids.append(result['id']['playlistId'])
-            #print(result['id']['playlistId'])
-    return playlists_ids
+class YTSearch:
 
+    def __init__(self):
+        self.api_key = 'AIzaSyC1GsiEj0D0zcQAyayaaCNa8oXeh9vU2bw'
+        self.yt = YouTubeDataAPI(self.api_key)
 
-def get_videos_from_playlist(playlist_id):
-    videos_ids = []
-    videos = yt.get_videos_from_playlist_id(playlist_id=playlist_id)
-    for video in videos:
-        videos_ids.append("https://www.youtube.com/watch?v="+video['video_id'])
-    return videos_ids
+    def get_playlists(self, key_word):  # key_word = co wyszukiwac czy spokojne czy jakies do rozruszenia XD
+        playlists_ids = []
+        playlists_links = []
+        if self.yt.verify_key():
+            results = self.yt.search(parser=None, q=key_word, max_results=5, search_type="playlist")
+            #print(results)
+            for result in results:
+                playlists_ids.append(result['id']['playlistId'])
+                playlists_links.append("https://www.youtube.com/playlist?list="+result['id']['playlistId'])
+        return playlists_ids, playlists_links
 
+    def get_videos_from_playlist(self, playlists_ids):
+        videos_ids = []
+        videos_links = []
+        for ids in playlists_ids:
+            videos = self.yt.get_videos_from_playlist_id(playlist_id=ids)
+            for video in videos:
+                videos_links.append("https://www.youtube.com/watch?v="+video['video_id'])
+                videos_ids.append(video['video_id'])
+        #unikalne wartosci:
+        videos_links = set(videos_links)
+        videos_ids = set(videos_ids)
+        return videos_ids, videos_links
 
-playlists = get_playlists()
-videos = get_videos_from_playlist(playlists[0]) #linki do muzyki z danej playlisty
+    def update(self): #odświerzanie danych raz na 24 h
+        #zapisz jako plik
+        playlists_ids, playlists_links = self.get_playlists("calm music")  # wypluwa id playlist i linki do nich
+        videos_ids, videos_links = self.get_videos_from_playlist(playlists_ids) #linki do muzyki z danej playlisty
+        print(videos_links)
+        print(videos_ids)
+        data = {'YT_data': []}
+        for ids, link in zip(videos_ids, videos_links):
+             data['YT_data'].append({
+                 'video_id' : ids,
+                 'video_link' : link
+             })
+        with open('YT_data.txt', 'w') as outfile:
+             json.dump(data, outfile)
+
+    # def choose_resuls(self):
+    # #     print("wylosowanie 3 muzyczek ")
+    # #
+    # # def get_response(self, pulse): # to bedziemy wywolywac!!!!
+    # #     response = []
+    # #     if pulse <= 60:
+    # #         print("za niski puls!")
+    # #     if pulse >= 85:
+    # #         print("za wysoki puls!!")
+    # #     if 60 < pulse < 85:
+    # #         print("Puls idealny :3 !")
+    # #     return response
 
 """""
+chcemy 3 piosenki spokojne i rock!!!! 
+ROZDZIELIĆ WYBOR CALM I ROCK I ZAPIS
+
+
 from youtube_search import YoutubeSearch
 import json
 
