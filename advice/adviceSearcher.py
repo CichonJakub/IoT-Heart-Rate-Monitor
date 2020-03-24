9c8c9e3e91c5ac26527321fa45f999daf52d427f
panda020 <matuszek.aleksandra@gmail.com>
2020-03-24 14:58:30 +0100

    wyszukiwanie porad

---
+++ advice/adviceSearcher.py
@@ -0,0 +1,31 @@
+import selenium.webdriver as webdriver
+
+
+def get_results(search_term):
+    url = "https://www.startpage.com"
+    browser = webdriver.Chrome("drivers\chromedriver.exe")
+    browser.get(url)
+    search_box = browser.find_element_by_id("query")
+    search_box.send_keys(search_term)
+    search_box.submit()
+    next = True
+    results = set()
+    while next:
+        links = browser.find_elements_by_xpath("//section//div//a[@class='w-gl__result-url']")
+
+        for link in links:
+            href = link.get_attribute("href")
+            results.add(href)
+
+        try:
+            browser.find_element_by_class_name("next").submit()
+        except:
+            next = False
+
+    browser.quit()
+    return results
+
+
+urls = get_results("jak obnizyc puls")
+for url in urls:
+    print(url)
