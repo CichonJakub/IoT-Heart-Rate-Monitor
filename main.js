41b41ae6a98968ded859b2a80ed22909c43d0960
Michal Ciborowski <mciborowski33@gmail.com>
2020-03-24 18:50:04 +0100

    fix #2

---
+++ main.js
@@ -0,0 +1,21 @@
+socket = io();
+
+socket.on('ack', function(data){
+    console.log(data);
+})
+
+socket.on('testMierzenie', function(data){
+    console.log("wyniki testu");
+    console.log(data);
+    let testWynik = document.getElementById('testWynik');
+    testWynik.innerHTML = data;
+})
+
+window.onload=function(){
+    socket.emit('hello');
+
+    document.getElementById('testMierzenie').addEventListener('click',function(){
+        console.log("test mierzenia...");
+        socket.emit('askForTest', "webBrowserTest");
+    })
+}
