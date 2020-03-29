82eaf2b5c9173f7430f1855b325c1d2f7d7f3070
rozanna <rozanna.boczar@gmail.com>
2020-03-29 14:51:36 +0200

    baza danych

--- crawler_api/Main.py
+++ crawler_api/Main.py
@@ -1,7 +1,86 @@
 #uaktualnij raz na 24 h
 #generuj odpowiedzi 5 obrazkow, 3 linki, porada
-import psycopg2
+import os
+import psycopg2 as pg
+from crawler_api.Pinterest_Crawler import *
+from crawler_api.YT_Search import *
+from crawler_api.adviceSearcher import *
 
-con = psycopg2.connect(database="iot", user="pulsometr", password="Marcin", host="localhost", port="5432")
+def update():
 
-print("Database opened successfully")
+    #  update obrazkow
+    p = PinCrawling()
+
+    p.login(p.browser)
+
+    low = ["horror"]
+    high = ["cute"]
+
+    pins_ids_low, pins_urls_low = p.get_data(p.browser, low)
+    p.update_data(pins_ids_low, pins_urls_low, "low")
+
+    pins_ids_high, pins_urls_high = p.get_data(p.browser, high)
+    p.update_data(pins_ids_high, pins_urls_high, "high")
+
+    p.browser.close()
+
+    #  update muzyki
+
+    yt = YTSearch()
+    yt_urls_high = yt.update("high")
+    yt_urls_low = yt.update("low")
+
+    return pins_urls_low, pins_urls_high, yt_urls_low, yt_urls_high
+
+
+def database():
+    DATABASE_URL = os.environ['DATABASE_URL'] = 'postgres://lweqbohgxkcciy:7141ba34c439114499df64f7c3e33db129f7961287f7dffa300255988e1fb029@ec2-176-34-97-213.eu-west-1.compute.amazonaws.com:5432/df7co2dencuc14'
+    conn = pg.connect(DATABASE_URL, sslmode='require')
+    c = conn.cursor()
+    postgres_insert_zdjecia = "INSERT INTO zdjecia (""kategoria"",""link"") VALUES (%s,%s)"
+    postgres_insert_muzyka = "INSERT INTO muzyka (""kategoria"",""link"") VALUES (%s,%s)"
+    postgres_insert_porady = "INSERT INTO porady (""kategoria"",""link"") VALUES (%s,%s)"
+    #c.execute("SELECT * FROM muzyka")
+    #eh = c.fetchall()
+    #print(eh)
+    low_photos, high_photos, low_music, high_music = update()
+    high_porady = get_results("jak obnizyc puls")
+    low_porady = get_results("jak podniesc tetno")
+
+    for l in low_photos:
+        record_to_insert = (0, l)
+        c.execute(postgres_insert_zdjecia, record_to_insert)
+        conn.commit()
+
+    for h in high_photos:
+        record_to_insert = (1, h)
+        c.execute(postgres_insert_zdjecia, record_to_insert)
+        conn.commit()
+
+    for l in low_music:
+        record_to_insert = (0, l)
+        c.execute(postgres_insert_muzyka, record_to_insert)
+        conn.commit()
+
+    for h in high_music:
+        record_to_insert = (1, h)
+        c.execute(postgres_insert_muzyka, record_to_insert)
+        conn.commit()
+
+    for l in low_porady:
+        record_to_insert = (0, l)
+        c.execute(postgres_insert_porady, record_to_insert)
+        conn.commit()
+
+    for h in high_porady:
+        record_to_insert = (1, h)
+        c.execute(postgres_insert_porady, record_to_insert)
+        conn.commit()
+
+def run():
+    print("IT WORKS")
+
+
+run()
+#database() jeszcze zeby sprawdzalo czy takie cos jest w bazie i ewentualnie dodac
+
---
+++ crawler_api/adviceSearcher.py
@@ -0,0 +1,33 @@
+import selenium.webdriver as webdriver
+from selenium.webdriver.support import ui
+
+
+def get_results(search_term):
+    url = "https://www.startpage.com"
+    #browser = webdriver.Chrome("drivers/chromedriver.exe")
+    browser = webdriver.Firefox(executable_path='Data/geckodriver-v0.26.0-linux64/geckodriver')
+    browser.get(url)
+    search_box = browser.find_element_by_id("query")
+    search_box.send_keys(search_term)
+    search_box.submit()
+    next = True
+    results = set()
+    while next:
+        links = browser.find_elements_by_xpath("//section//div//a[@class='w-gl__result-url']")
+
+        for link in links:
+            href = link.get_attribute("href")
+            results.add(href)
+
+        try:
+            browser.find_element_by_class_name("next").submit()
+        except:
+            next = False
+
+    browser.quit()
+    return results
+
+
+# urls = get_results("lol")
+# for url in urls:
+#     print(url)
