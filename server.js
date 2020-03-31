240b648791d6ee02b5b99aabfac77a6538c168c5
Michal Ciborowski <mciborowski33@gmail.com>
2020-03-31 19:01:45 +0200

    new sockets connection

--- server.js
+++ server.js
@@ -61,10 +61,12 @@ io.on('connection', function (socket) {
 
     console.log("CONNECTED");
     socket.on('hello', function(data){
-        let tmpId = 1;
-        if( data == "webBrowser" )
+        //let tmpId = 1;
+        if( data == "webBrowser" ){
             tmpId = 666;
-        users.push({id: tmpId, socket: socket});
+            users.push({id: tmpId, socket: socket});
+            }
+        //users.push({id: tmpId, socket: socket});
         console.log(data);
         socket.emit('ack', "TEST");
         console.log("HELLO RECEIVED");
@@ -72,7 +74,7 @@ io.on('connection', function (socket) {
 
     socket.on('iAmHardware', function(data){
         socketHardware = socket;
-        users.push({id: 0, socket: socket});
+        //users.push({id: 0, socket: socket});
     })
 
     socket.on("askForTest", function(data){
@@ -90,19 +92,55 @@ io.on('connection', function (socket) {
         console.log(data.pomiar);
         console.log("connected sockets number: " + users.length);
         let index = users.findIndex(obj => obj.id == data.user);
-        if( index != -1 ){
+        if( index != -1 ){/*
             var token = new fernet.Token({
                 secret: secret,
                 token: data.pomiar,
                 ttl: 0
             })
-            let pomiar = token.decode();
-            users[index].socket.emit('testMierzenie', pomiar);
+            let pomiar = token.decode();*/
+            users[index].socket.emit('testMierzenie', data.pomiar);
         }
-        else{//czy to działa?
+        else{
             console.log("Something went wrong, this id is not connected");
         }
     })
+
+    socket.on('register', function(data){
+        socket.emit('confirmRegister', false);
+    });
+
+    socket.on('login', function(data){
+        console.log(data);
+        users.push({id: data, socket: socket});
+        socket.emit('confirmLogin', "Test login id=" + data);
+    });
+
+    socket.on('requestPomiar', function(data){
+        //console.log("askForTest: " + data);
+        let index = users.findIndex(obj => obj.socket == socket);
+        if( socketHardware == null ){
+            console.log("NO DEVICE");
+            socket.emit('requestPomiar2', "FAILEDTOMEASURE");
+        }
+        else if( index == -1 ){
+            console.log("USER ERROR");
+            socket.emit('requestPomiar2', "FAILEDTOMEASURE");
+        }
+        else{
+            let userId = users[index].id;
+            console.log("Relaying ask for test for " + userId);
+            socketHardware.emit("askForTest", userId);
+        }
+    });
+
+    socket.on('pomiarResults', function(data){
+        let index = users.findIndex(obj => obj.id == data.id);
+        if( index != -1 ){
+            users[index].socket.emit('pomiarResults2', data.pomiar);
+        }
+    });
+
 
     socket.on('database_porady', function(data){
         let valuesString = '';
@@ -128,10 +166,15 @@ io.on('connection', function (socket) {
     })
 
     socket.on('disconnect', function(data){
-            //console.log("Disconnect");
-            let index = users.findIndex(obj => obj.socket == socket);
-            if( index != -1 ){
-                users.splice(index, 1);
+            console.log("Disconnect");
+            if( socketHardware == socket ){
+                socketHardware = null;
+            }
+            else{
+                let index = users.findIndex(obj => obj.socket == socket);
+                if( index != -1 ){
+                    users.splice(index, 1);
+                }
             }
         })
 
