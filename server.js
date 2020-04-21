01462e9cf041cceb44f7baa54cdcb405dd3e674e
Michal Ciborowski <mciborowski33@gmail.com>
2020-04-21 19:11:33 +0200

    update

--- server.js
+++ server.js
@@ -407,7 +407,7 @@ io.on('connection', function (socket) {
                 users[index].socket.emit('pomiarResult2', {pomiar: pomiar});
             }
         }
-        if else( pomiar >= 65 && pomiar < 85 ){
+        if else( pomiar >= 75 && pomiar < 85 ){
             index = users.findIndex(obj => obj.id == data.user);
             if( index != -1 ){
                 console.log("SENDING RESULTS...");
