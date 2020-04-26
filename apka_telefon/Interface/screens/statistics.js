3372538ce5f86808b38697b7371c50924bd6f53d
Aleksandra <49525674+panda020@users.noreply.github.com>
2020-04-26 21:37:40 +0200

    formatowanie kodu

--- apka_telefon/Interface/screens/statistics.js
+++ apka_telefon/Interface/screens/statistics.js
@@ -3,7 +3,6 @@ import { View, Text, StyleSheet, Dimensions, ScrollView } from 'react-native';
 import { LineChart } from 'react-native-chart-kit';
 import { statystykiSrednia, statystykiPomiary } from './home';
 import { styles } from '../styles/global';
-//import { VictoryLine, VictoryChart, VictoryTheme, VictoryLabel } from "victory-native";
 
  export default function Statistics() {
 
@@ -22,12 +21,12 @@ import { styles } from '../styles/global';
 	dni_tygodnia[6] = "Sob";
 
 	let dni = [
-		dni_tygodnia[(dzis.getDay()-6+7) % 7],
-		dni_tygodnia[(dzis.getDay()-5+7) % 7],
-		dni_tygodnia[(dzis.getDay()-4+7) % 7],
-		dni_tygodnia[(dzis.getDay()-3+7) % 7],
-		dni_tygodnia[(dzis.getDay()-2+7) % 7],
-		dni_tygodnia[(dzis.getDay()-1+7) % 7],
+		dni_tygodnia[(dzis.getDay()+1) % 7],
+		dni_tygodnia[(dzis.getDay()+2) % 7],
+		dni_tygodnia[(dzis.getDay()+3) % 7],
+		dni_tygodnia[(dzis.getDay()+4) % 7],
+		dni_tygodnia[(dzis.getDay()+5) % 7],
+		dni_tygodnia[(dzis.getDay()+6) % 7],
 		dni_tygodnia[dzis.getDay()]
 		]
 	
@@ -61,104 +60,95 @@ import { styles } from '../styles/global';
 		}
 	 }
 
-
-
-
-
 	 if(statystykiPomiary != undefined){
 	 	for(let i=0; i < statystykiPomiary.length; i++){
 	 		pomiary_dzisiaj[i] = statystykiPomiary[i].wartosc;
 	 		nr_pomiaru[i] = i+1;
 	 	}
-
 	 }
-
-
 
 	return(
 		<ScrollView>
+			<Text style={styles.s1}> Dzisiejsze pomiary </Text>
+			<LineChart
+			    data={{
+			      labels: nr_pomiaru,
+			      datasets: [
+			        {
+			          data: pomiary_dzisiaj
+			        }
+			      ]
+			    }}
+			    width={Dimensions.get("window").width}
+			    height={Dimensions.get("window").height/2-50}
+			    fromZero 
+			    yAxisLabel=""
+			    yAxisSuffix=""
+			    yAxisInterval={1} // optional, defaults to 1
+			    chartConfig={{
+			      backgroundColor: '#f2f2f2',
+			      backgroundGradientFrom: '#f2f2f2',
+			      backgroundGradientTo: '#f2f2f2',
+			      decimalPlaces: 2, // optional, defaults to 2dp
+			      color: (opacity = 1) => `rgba(140,33,85, ${opacity})`,
+			      labelColor: (opacity = 1) => `rgba(90,0,44, ${opacity})`,
+			      style: {
+			        borderRadius: 16,
+			      },
+			      propsForDots: {
+			        r: "6",
+			        strokeWidth: "2",
+			        stroke: "#5a002c"
+			      }
+			    }}
+			    bezier
+			    style={{
+			      marginVertical: 10,
+			    }}
+			/>
 
-    <View>
-    <Text style={styles.s1}> Dzisiejsze pomiary </Text>
-    <LineChart
-	    data={{
-	      labels: nr_pomiaru,
-	      datasets: [
-	        {
-	          data: pomiary_dzisiaj
-	        }
-	      ]
-	    }}
-	    width={Dimensions.get("window").width}
-	    height={Dimensions.get("window").height/2-50}
-		fromZero
-		yAxisLabel=""
-	    yAxisSuffix=""
-	    yAxisInterval={1} // optional, defaults to 1
-	    chartConfig={{
-	      backgroundColor: '#f2f2f2',
-	      backgroundGradientFrom: '#f2f2f2',
-	      backgroundGradientTo: '#f2f2f2',
-	      decimalPlaces: 2, // optional, defaults to 2dp
-	      color: (opacity = 1) => `rgba(140,33,85, ${opacity})`,
-	      labelColor: (opacity = 1) => `rgba(90,0,44, ${opacity})`,
-	      style: {
-	        borderRadius: 16,
-	      },
-	      propsForDots: {
-	        r: "6",
-	        strokeWidth: "2",
-	        stroke: "#5a002c"
-	      }
-	    }}
-	    bezier
-	    style={{
-	      marginVertical: 10,
-	    }}
-	  />
-
-  <Text style={styles.s1}> Średnie wyniki pomiarów z ostatnich 7 dni </Text>
-	  <LineChart
-	    data={{
-	      labels: dni,
-	      datasets: [
-	        {
-	          data: [
-	            srednie[0],
-	            srednie[1],
-	            srednie[2],
-	            srednie[3],
-	            srednie[4],
-	            srednie[5],
-	            srednie[6]
-	          ]
-	        }
-	      ]
-	    }}
-	    width={Dimensions.get("window").width}
-	    height={Dimensions.get("window").height/2-50}
-	    yAxisLabel=""
-	    yAxisSuffix=""
-	    yAxisInterval={1} // optional, defaults to 1
-	    chartConfig={{
-	      backgroundColor: '#f2f2f2',
-	      backgroundGradientFrom: '#f2f2f2',
-	      backgroundGradientTo: '#f2f2f2',
-	      decimalPlaces: 2, // optional, defaults to 2dp
-	      color: (opacity = 1) => `rgba(140,33,85, ${opacity})`,
-	      labelColor: (opacity = 1) => `rgba(90,0,44, ${opacity})`,
-	      style: {
-	        borderRadius: 16,
-	      },
-	      propsForDots: {
-	        r: "6",
-	        strokeWidth: "2",
-	        stroke: "#5a002c"
-	      }
-	    }}
-	    bezier
-	  />
-	</View>
-	</ScrollView>
+			<Text style={styles.s1}> Średnie wyniki pomiarów z ostatnich 7 dni </Text>
+			<LineChart
+			    data={{
+			      labels: dni,
+			      datasets: [
+			        {
+			          data: [
+			            srednie[0],
+			            srednie[1],
+			            srednie[2],
+			            srednie[3],
+			            srednie[4],
+			            srednie[5],
+			            srednie[6]
+			          ]
+			        }
+			      ]
+			    }}
+			    width={Dimensions.get("window").width}
+			    height={Dimensions.get("window").height/2-50}
+			    yAxisLabel=""
+			    yAxisSuffix=""
+			    yAxisInterval={1} 
+			    chartConfig={{
+			      backgroundColor: '#f2f2f2',
+			      backgroundGradientFrom: '#f2f2f2',
+			      backgroundGradientTo: '#f2f2f2',
+			      decimalPlaces: 2, 
+			      color: (opacity = 1) => `rgba(140,33,85, ${opacity})`,
+			      labelColor: (opacity = 1) => `rgba(90,0,44, ${opacity})`,
+			      style: {
+			        borderRadius: 16,
+			      },
+			      propsForDots: {
+			        r: "6",
+			        strokeWidth: "2",
+			        stroke: "#5a002c"
+			      },
+			    }}
+			    bezier
+			  />
+		</ScrollView>
 	)
 }
+
